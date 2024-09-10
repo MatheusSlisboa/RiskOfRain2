630c5f7ea790a9c8f80351827ecab7e20e2e23bc
Matheus Sales <113493746+MatheusSlisboa@users.noreply.github.com>
2024-09-10 00:19:19 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {    
+    titulo : 'Commando',
+    descricao : 'Desbloqueado desde o início, Commando é um personagem versátil, projetado para ser acessível a todos os jogadores.Sua combinação de agilidade e força ofensiva o torna uma opção sólida para qualquer estilo de jogo, sem se destacar em nenhuma área específica.',
+    link : "https://riskofrain2.fandom.com/wiki/Commando",
+    tags: "inicial versatil",
+    },
+    {    
+    titulo : 'Huntress',
+    descricao : "Huntress é um dos personagens desbloqueados no início do jogo. Seus ataques teleguiados e grande mobilidade a permitem manter distância dos inimigos, compensando sua baixa vida. A habilidade de desviar é crucial para seu estilo de jogo",
+    link : "https://riskofrain2.fandom.com/wiki/Huntress",
+    tags: "caçadora arco flecha mira",
+    },
+    {    
+    titulo : 'Bandit',
+    descricao : 'Bandit é um personagem que recompensa a habilidade do jogador. Sua jogabilidade se baseia em executar combos fluidos, alternando entre a forma camuflada para surpreender os inimigos e realizar ataques devastadores pelas costas.',
+    link : "https://riskofrain2.fandom.com/wiki/Bandit",
+    tags: "invisibilidade furtivo sniper",
+    },
+];
+
+ 
+   
